b904011c469e54502696988c9329b91956074ace
lindabell <lindabell@163.com>
2015-06-02 11:22:59 +0800

    增加百度统计代码

--- js/move-top.js
+++ js/move-top.js
@@ -3,3 +3,14 @@
 if(sd>settings.min)
 $(containerIDhash).fadeIn(settings.inDelay);else
 $(containerIDhash).fadeOut(settings.Outdelay);});};})(jQuery);
+
+/*增加百度统计*/
+/*
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?c5db5dff5dc004d8857ab6ef4ef9bf3b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+*/
