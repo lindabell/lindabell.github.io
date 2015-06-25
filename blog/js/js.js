05f07576836cda52db5ab2391997dd1ea412de64
lindabell <lindabell@163.com>
2015-06-25 20:36:02 +0800

    添加百度统计代码到blog中

--- blog/js/js.js
+++ blog/js/js.js
@@ -17,3 +17,15 @@ $(document).ready(function(e) {
 $(window).scroll(function(e){
 	b();		
 })
+
+
+/*增加百度统计*/
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?c5db5dff5dc004d8857ab6ef4ef9bf3b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
