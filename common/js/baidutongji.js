eb7ad15a3a83d4cd0d2c5c529dc7430da7253180
lindabell <lindabell@163.com>
2015-06-28 14:47:57 +0800

    把百度统计代码单独出来，并添加到页面测试

---
+++ common/js/baidutongji.js
@@ -0,0 +1,9 @@
+/*百度统计代码*/
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?c5db5dff5dc004d8857ab6ef4ef9bf3b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
